b064879cfa714dbe8e0713bbe0b939ce1f87d392
ch4perone <ch4perone@gmail.com>
2024-08-01 21:15:23 +0200

    WIP: Implement board and hand mechanics (3: some error poping up)

--- src/card_states/card_state.gd
+++ src/card_states/card_state.gd
@@ -4,7 +4,7 @@ extends Node
 enum State {BASE, CLICKED, DRAGGING, AIMING, RELEASED}
 
 @export var state: State
-@export var cardui: CardUI
+@export var card_ui: CardUI
 
 func enter() -> void:
 	pass
@@ -22,14 +22,4 @@ func on_mouse_entered() -> void:
 	pass
 
 func on_mouse_exited() -> void:
-	pass
-
-
-# Called when the node enters the scene tree for the first time.
-func _ready():
-	pass # Replace with function body.
-
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta):
 	pass
